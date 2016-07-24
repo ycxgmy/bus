62783d4d01bc29d120072cc523b94943c7905175
wkk <121417345@qq.com>
2016-07-24 14:34:19 +0800

    add argv process

--- test.cpp
+++ test.cpp
@@ -12,21 +12,39 @@ char * mat2jpgbuf(const cv::Mat &m, int &buflen) {
   return p;
 }
 int main(int argc, char *argv[]) {
-  Session *ss = new Session("67890"
-			    ,"四川省"
-			    ,"绵阳市"
-			    ,"高新区"
-			    ,"801"
-			    ,"王王"
+	string plateNumber = "67890"; if (argc > 1) plateNumber = argv[1];
+	string province = "四川省";   if (argc > 2) province = argv[2];
+	string city = "绵阳市";       if (argc > 3) city = argv[3];
+	string county = "高新区";     if (argc > 4) county = argv[4];
+	string busRoute = "801";      if (argc > 5) busRoute = argv[5];
+	string driverName = "王王";   if (argc > 6) driverName = argv[6];
+	string username = "test";     if (argc > 7) username = argv[7];
+	string password = "123456";   if (argc > 8) password = argv[8];
+	string verbose = "1";         if (argc > 9) verbose = argv[9];
+	string imagefile = "";        if (argc > 10) imagefile = argv[10];
+  Session *ss = new Session(plateNumber
+			    , province
+			    , city
+			    , county
+			    , busRoute
+			    , driverName
+				, username
+				, password
+				, std::stoi(verbose)
 			    );
 
   for (int i = 0; i < 1; ++i) {
-    Mat x = Mat::zeros(100,100, CV_8UC1);
+	  Mat x;
+	  x = cv::imread(imagefile);
+	  if (x.empty()) {
+		  x = Mat::zeros(100, 100, CV_8UC1);
+	  }
 	putText(x, std::to_string(cv::getTickCount()), cv::Point(0, 50), 1, 1, cv::Scalar(255, 255, 255));
 
     int buflen;  char *pp = mat2jpgbuf(x, buflen);
     ss->alarm_face(pp, buflen, 0.9);
   }
   delete ss;
+  system("pause");
   return 0;
 }
